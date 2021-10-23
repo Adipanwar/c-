c3e27dc18aae858623ab5910b59340f1920bc224
Adipanwar <92621936+Adipanwar@users.noreply.github.com>
2021-10-23 10:37:34 +0530

    Add files via upload

---
+++ practice if or else.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+int age;
+printf("enter your age");
+scanf("%d",age);
+if(age>18){
+    printf("you can vote");
+}
+else if(age>10){
+    printf("you can not even smile");
+}
+else{
+    ("you cannot vote");
+}
+return 0;
+}
